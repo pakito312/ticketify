d1199aa4b5b944feced36c5f1883cb5fa5d48904
pakito312 <40355919+pakito312@users.noreply.github.com>
2025-01-05 22:49:03 +0100

    update

--- src/Providers/TicketifyServiceProvider.php
+++ src/Providers/TicketifyServiceProvider.php
@@ -7,15 +7,15 @@ class TicketifyServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/ticketify.php', 'ticketify');
+        $this->mergeConfigFrom(__DIR__.'/../../config/ticketify.php', 'ticketify');
     }
 
     public function boot()
 {
     $this->publishes([
-        __DIR__.'/../config/ticketify.php' => config_path('ticketify.php'),
+        __DIR__.'/../../config/ticketify.php' => config_path('ticketify.php'),
         __DIR__.'/../resources/views' => resource_path('views/vendor/ticketify'),
-        __DIR__.'/../database/migrations/' => database_path('migrations'),
+        __DIR__.'/../../database/migrations/' => database_path('migrations'),
     ], 'ticketify-resources');
 
     $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
